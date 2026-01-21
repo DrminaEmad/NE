a447e7dabf514510476f050a20653ffbace95ce7
DrminaEmad <drmina200015@gmail.com>
2026-01-21 13:17:05 +0000

    chapter 11 part 2

--- nextjs-dashboard/next.config.ts
+++ nextjs-dashboard/next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+    experimental: {
+    serverActions: {
+      allowedOrigins: [
+        'super-duper-happiness-6947q5r7g5v725qjg-3000.app.github.dev',
+        'localhost:3000',
+        '*.app.github.dev'
+      ],
+    },
+  },
 };
 
 export default nextConfig;
